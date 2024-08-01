4a673ad5da7e252956c70768b4db909d488c6224
JuampiConejera <juampiconejera@gmail.com>
2024-08-01 13:14:03 -0300

    update ejercicio6.pas

--- Taller/Practicas/practica-2/ejercicio6.pas
+++ Taller/Practicas/practica-2/ejercicio6.pas
@@ -2,7 +2,7 @@ program ejercicio6;
 procedure binario(num: integer);
 begin
   if(num > 0) then begin
-    binario(num);
+    binario(num div 2);
     Write(num MOD 2);
   end;
 end;
@@ -10,9 +10,11 @@ end;
 var
   num: integer;
 begin
-  WriteLn('Numero entero');
+  Write('Numero entero: '); ReadLn(num);
   while(num <> 0) do begin
-    
+    binario(num);
+    WriteLn;
+    Write('Numero entero: '); ReadLn(num);
   end;
 end.
 {6.- Realizar un programa que lea números y que utilice un módulo recursivo que escriba el
