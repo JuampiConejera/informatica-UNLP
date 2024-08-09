16c5852be39fc4bc0c1224957627616062c1333b
JuampiConejera <juampiconejera@gmail.com>
2024-08-09 17:26:38 -0300

    Create ejercicio6-a.asm

---
+++ AC/Resuelto/ejercicio6-a.asm
@@ -0,0 +1,17 @@
+      ORG 1000H
+      NUM1 DW 3
+      NUM2 DW 0
+      RES DW ?
+      ORG 2000H
+      ;INICIALIZO AX,BX
+      MOV AX, 0
+      MOV BX, NUM2
+      CMP BX, 0
+      JZ FIN
+LOOP: ADD AX, NUM1
+      DEC BX
+      CMP BX, 0
+      JNZ LOOP
+FIN:  MOV RES, AX
+      HLT
+      END
