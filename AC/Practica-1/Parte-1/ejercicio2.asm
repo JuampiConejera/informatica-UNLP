799fce64dae87e8c1ca2edb3e0fef1cf4fdf398b
JuampiConejera <juampiconejera@gmail.com>
2024-08-16 20:03:39 -0300

    aa

---
+++ AC/Practica-1/Parte-1/ejercicio2.asm
@@ -0,0 +1,13 @@
+        ORG 1000H
+        C DB "@"
+        RES DB ?
+        ORG 2000H
+        CMP C, 90
+        JNS ELSE
+        CMP C, 65
+        JNS THEN
+ELSE:   MOV RES, 0
+        JMP FIN
+THEN:   MOV RES, 0FFH
+FIN:    HLT
+        END
