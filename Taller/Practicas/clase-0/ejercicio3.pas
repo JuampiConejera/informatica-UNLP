8481e4a80fc79bb16a3dd9755385a07039691336
JuampiConejera <juampiconejera@gmail.com>
2024-08-21 13:01:43 -0300

    Update ejercicio3.pas

--- Taller/Practicas/clase-0/ejercicio3.pas
+++ Taller/Practicas/clase-0/ejercicio3.pas
@@ -6,10 +6,11 @@ Type
     sig: lista;
   end;
 
-procedure agregarAdelante(l: lista;n: integer);
+procedure agregarAdelante(var l: lista;n: integer);
 var
   aux: lista;
 begin
+  new(aux);
   aux^.dato := n;
   aux^.sig := l;
   l := aux;
@@ -48,22 +49,21 @@ var
   l: lista;
   numBuscar: integer;
 begin
+  randomize;
   l := Nil;
   cargarLista(l);
-  WriteLn('pasa');
+  ImprimirLista(l);
   WriteLn('Numero para buscar en la lista: ');ReadLn(numBuscar);
-  
+  WriteLn(BuscarElemento(l,numBuscar));
 end.
 
 {ACTIVIDAD 3: Crear un archivo ProgramaListas.pas
 a) Implemente un módulo CargarLista que cree una lista de enteros y le
 agregue valores aleatorios entre el 100 y 150, hasta que se genere el 120.
 b) Implemente un módulo ImprimirLista que reciba una lista generada en a) 
-e imprima todos los valores de la lista en el mismo orden que están
-almacenados.
+e imprima todos los valores de la lista en el mismo orden que están almacenados.
 c) Implemente un módulo BuscarElemento que reciba la lista generada
-en a) y un valor entero y retorne true si el valor se encuentra en la lista
-y false en caso contrario.
+en a) y un valor entero y retorne true si el valor se encuentra en la lista y false en caso contrario.
 d) Invocar desde el programa principal a los módulos implementados
 para crear una lista, mostrar todos sus elementos y determinar si un
 valor leído por teclado se encuentra o no en la lista.}
