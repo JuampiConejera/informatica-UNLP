03a0008adcc0c9875f6981c45c919969740748ba
JuampiConejera <juampiconejera@gmail.com>
2024-08-22 14:40:51 -0300

    Update ejercicio4.pas

--- Taller/Practicas/clase-0/ejercicio4.pas
+++ Taller/Practicas/clase-0/ejercicio4.pas
@@ -14,7 +14,7 @@ begin
   nuevo^.dato := n;
   actual := l;
   anterior := l;
-  while(l <> Nil) and (n < actual^.dato) do begin
+  while((actual <> Nil) and (n > actual^.dato)) do begin
     anterior := actual;
     actual := actual^.sig;
   end;
@@ -25,7 +25,7 @@ begin
   nuevo^.sig := actual;
 end;
 
-procedure cargarListaOrdenada(l: lista);
+procedure cargarListaOrdenada(var l: lista);
 var
   num: integer;
 begin
@@ -65,18 +65,6 @@ begin
   cargarListaOrdenada(l);
   WriteLn('pasa');
   imprimirLista(l);
-  Write('Numero a buscar: ', numBuscar);
-  BuscarElementoOrdenado(l,numBuscar);
+  Write('Numero a buscar: '); readln(numBuscar);
+  writeln(BuscarElementoOrdenado(l,numBuscar));
 end.
-{ACTIVIDAD 4: Crear un archivo ProgramaListasOrdenadas.pas
-a) Implemente un módulo CargarListaOrdenada que cree una lista de
-enteros y le agregue valores aleatorios entre el 100 y 150, hasta que se
-genere el 120. Los valores dentro de la lista deben quedar ordenados de menor a mayor.
-b) Reutilice el módulo ImprimirLista que reciba una lista generada en a) e
-imprima todos los valores de la lista en el mismo orden que están almacenados.
-c) Implemente un módulo BuscarElementoOrdenado que reciba la lista
-generada en a) y un valor entero y retorne true si el valor se encuentra
-en la lista y false en caso contrario.
-d) Invocar desde el programa principal a los módulos implementados
-para crear una lista ordenada, mostrar todos sus elementos y
-determinar si un valor leído por teclado se encuentra o no en la lista.}
