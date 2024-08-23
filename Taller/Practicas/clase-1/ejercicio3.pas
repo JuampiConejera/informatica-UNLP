2091ea16d54483dbabde533e1f0ba8e34b0b07ed
JuampiConejera <juampiconejera@gmail.com>
2024-08-23 14:43:05 -0300

    aparentemente fixeado

--- Taller/Practicas/clase-1/ejercicio3.pas
+++ Taller/Practicas/clase-1/ejercicio3.pas
@@ -1,6 +1,8 @@
 program ejercicio3;
+const
+  dimF = 8;
 type
-  subrangoPeliculas = 1..8;
+  subrangoPeliculas = 1..dimF;
   pelicula = record
     codigoPelicula: integer;
     codigoGenero: subrangoPeliculas;
@@ -12,7 +14,7 @@ type
     sig: lista;
   end;
   vector = array[subrangoPeliculas] of lista;
-  vectorMaximo = array[subrangoPeliculas] of integer;
+  VectorMaximo = array[subrangoPeliculas] of pelicula;
 procedure leerPelicula(var p: pelicula);
 begin
   write('Codigo pelicula: ');{eadLn(p.codigoPelicula);}p.codigoPelicula := Random(100)-1;
@@ -56,37 +58,36 @@ begin
   end;
 end;
 
-procedure cargarvectorMaximo(var vm: vectorMaximo;vp: vector);
+procedure cargarVectorMaximo(var vm: vectorMaximo;vp: vector);
 var
   i: integer;
   maximo: real;
 begin
-  for i := 1 to 8 do begin
+  for i := 1 to dimF do begin
     maximo := -1;
     while(vp[i] <> Nil) do begin
       if(maximo < vp[i]^.dato.puntajePromedio) then begin
         maximo := vp[i]^.dato.puntajePromedio;
-        vm[i] := vp[i]^.dato.codigoPelicula;
+        vm[i] := vp[i]^.dato;
       end;
       vp[i] := vp[i]^.sig;
     end;
-    WriteLn(maximo:2:2,' ',vm[i]);
   end;
 end;
 
 procedure seleccion(var v: vectorMaximo);
 var
   i,j,pos: integer;
-  item: integer;
+  item: pelicula;
 begin
-  for i := 1 to 7 do begin
+  for i := 1 to dimF-1 do begin
     pos := i;
     for j := i+1 to 8 do
-      if(v[j] < v[pos]) then
+      if(v[j].puntajePromedio > v[pos].puntajePromedio) then
         pos := j;
-    item := v[pos];
-    v[pos] := v[i];
-    v[i] := item;
+      item := v[pos];
+      v[pos] := v[i];
+      v[i] := item;
   end;
 end;
 
@@ -96,25 +97,8 @@ var
 begin
   randomize;
   cargarVector(v);
-  WriteLn;
-  WriteLn;
-  WriteLn;
-  cargarvectorMaximo(vm,v);
+  cargarVectorMaximo(vm,v);
   seleccion(vm);
-  WriteLn('Mayor puntaje:', vm[1]);
-  WriteLn('menor puntaje:', vm[8]);
+  WriteLn('Mayor puntaje: ',vm[1].puntajePromedio:0:1,'. Codigo pelicula: ',vm[1].codigoPelicula);
+  WriteLn('Menor puntaje: ',vm[8].puntajePromedio:0:1,'. Codigo pelicula: ',vm[8].codigoPelicula);
 end.
-{3.- Netflix ha publicado la lista de películas que estarán disponibles durante el mes de 
-diciembre de 2022. De cada película se conoce: código de película, código de género (1: acción, 
-2: aventura, 3: drama, 4: suspenso, 5: comedia, 6: bélico, 7: documental y 8: terror) y puntaje 
-promedio otorgado por las críticas. 
-Implementar un programa que invoque a módulos para cada uno de los siguientes puntos:
-a. Lea los datos de películas, los almacene por orden de llegada y agrupados por código de 
-género, y retorne en una estructura de datos adecuada. La lectura finaliza cuando se lee el 
-código de la película -1. 
-b. Genere y retorne en un vector, para cada género, el código de película con mayor puntaje 
-obtenido entre todas las críticas, a partir de la estructura generada en a)..
-c. Ordene los elementos del vector generado en b) por puntaje utilizando alguno de los dos 
-métodos vistos en la teoría. 
-d. Muestre el código de película con mayor puntaje y el código de película con menor puntaje, 
-del vector obtenido en el punto c).}
