4d5a2c0962b17f8c32e8596533b5756dc5b9ef16
JuampiConejera <juampiconejera@gmail.com>
2024-10-30 17:31:59 -0300

    COSITAS

--- AC/WinMIPS/practica-4/parte-2/ejercicio2.s
+++ AC/WinMIPS/practica-4/parte-2/ejercicio2.s
@@ -4,21 +4,44 @@ DATA: .word 0x10008
 texto: .asciiz "Ingrese una clave de 4 caracteres:"
 acierto: .asciiz "Clave correcta: acceso permitido"
 error: .asciiz "Clave incorrecta."
-clave: .ascii "1234"
+clave: .asciiz "ABCD"
+leido: .asciiz ""
 
 .code
-lwu $s0, CONTROL ($zero)        
-lwu $s1, DATA ($zero)           
-daddi $t0, $zero, 0             ; puntero para recorrer 
+ld $s0, CONTROL ($zero)        
+ld $s1, DATA ($zero)           
 
 daddi $t9, $zero, texto         ; guardo la dir TEXTO
 sd $t9, 0 ($s1)                 ; DATA recibe el puntero del comienzo del msj
 daddi $t9, $zero, 4             ; numero para CONTROL (salida msj)
 sd $t9, 0 ($s0)                 ; CONTROL recibe 4 y hace función
 
-loop:
-    daddi $t3, $zero, 9         ; guardo 9 para enviar a CONTROL
-    sd $t3, 0 ($s0)             ; envio 9 a CONTROL
-    lbu $t3, 0 ($s1)            ; cargo el caracter desde DATA
+daddi $t1, $zero, 9 
+daddi $t7, $zero, 4             ;contador (-1)
+daddi $t4, $zero, 0             ;PUNTERO PARA CARGAR
+carga: sd $t1, 0 ($s0)                 ;pido un char
 
+ld $t3, 0 ($s1)                 
+sb $t3, leido ($t4)             ;guardo el char
+
+daddi $t4, $t4, 1
+daddi $t7, $t7, -1
+bnez $t7, carga
+
+ld $t0, clave ($zero)
+ld $t1, leido ($zero)
+
+beq $t0, $t1, correcto          ;clave = leido CORRECTO
+
+ld $t5, error ($zero)
+sd $t5, 0 ($s1)
+j fin
+
+correcto: ld $t5, acierto ($zero)
+sd $t5, 0 ($s1)
+
+fin: sd $t9, 0 ($s0)
+
+daddi $t1, $zero, 7
+sd $t1, 0 ($s0)
 halt
